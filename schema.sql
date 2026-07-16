995b17fb02d9f11c5f985f0d4dac641b29b9edc3
Aman <aj007.12@gmail.com>
2026-07-16 14:49:45 +0530

    Fix autograder title and UMSI login

--- schema.sql
+++ schema.sql
@@ -33,3 +33,6 @@ CREATE TABLE `Position` (
 
 INSERT INTO users (name, email, password)
 VALUES ('Chuck', 'csev@umich.edu', '1a52e17fa899cf40fb04cfc42e6352f1');
+
+INSERT INTO users (name, email, password)
+VALUES ('UMSI', 'umsi@umich.edu', '1a52e17fa899cf40fb04cfc42e6352f1');
